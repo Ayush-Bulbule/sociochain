ceb5b7ac336abd1855b6d2528f70c9775729e2ff
Ayush-Bulbule <ayushbulbule24@gmail.com>
2023-05-10 00:32:41 +0530

    Fixex post padding

--- frontend/components/PostCard1.js
+++ frontend/components/PostCard1.js
@@ -5,9 +5,9 @@ export default function PostCard() {
     return (
 
         <div className="avatar  space-y-6 items-start m-3">
-            <div className=" w-full flex my-2 flex-row bg-white p-4 border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-slate-700">
-                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className="w-10 h-10" />
-                <div className="flex flex-col pl-4 pr-2">
+            <div className=" w-full flex my-2 flex-row bg-white p-3 sm:p-4 border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-slate-700">
+                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className="w-8 h-8 sm:w-10 sm:h-10" />
+                <div className="flex flex-col pl-2 sm:pl-4 pr-2">
                     <div className="avatar flex items-center">
                         <h6 className="text-sm font-semibold tracking-tight text-gray-950 dark:text-white">
                             Durvesh Chopade
@@ -20,15 +20,15 @@ export default function PostCard() {
                             <HiOutlineHeart />
                             <p className="text-xs px-2 ">Like</p>
                         </div>
-                        <div className="action md:px-3 text-gray-300 hover:text-rose-400 flex cursor-pointer">
+                        <div className="action md:px-3 text-gray-300 hover:text-green-400 flex cursor-pointer">
                             <HiOutlineRefresh />
                             <p className="text-xs px-2">ReShare</p>
                         </div>
-                        <div className="action md:px-3 text-gray-300 hover:text-rose-400 flex cursor-pointer">
+                        <div className="action md:px-3 text-gray-300 hover:text-cyan-400 flex cursor-pointer">
                             <HiOutlineShare />
                             <p className="text-xs px-2">Share</p>
                         </div>
-                        <div className="action md:px-3 text-gray-300 hover:text-rose-400 flex cursor-pointer">
+                        <div className="action md:px-3 text-gray-300 hover:text-yellow-200 flex cursor-pointer">
                             <HiOutlineFlag />
                             <p className="text-xs px-2">Flag</p>
                         </div>
