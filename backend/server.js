66cbf9419dabec811ec988f23d0b4de6a3e89f90
Ayush-Bulbule <ayushbulbule24@gmail.com>
2023-05-11 00:06:34 +0530

    Added pseudo login

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
+const cors = require('cors')
+
 const User = require('./model/user')
 const app = express()
 
-
 app.use(express.json())
+app.use(cors())
+
 
 //Database connection
 mongoose.connect('mongodb+srv://ayushbulbule24:IwXN64MhYG1YckhX@cluster0.etqgjcy.mongodb.net/?retryWrites=true&w=majority',
@@ -28,45 +31,45 @@ app.get('/', (req, res) => {
 });
 
 //Get All Users ----------------------------------------------------------------------------------
-app.get("/api/all",async (req,res)=>{
-    const users = await User.find().then(users=>{
-        res.json({"users":users})
-    }).catch(err=>{
-        res.json({"error":err})
+app.get("/api/all", async (req, res) => {
+    const users = await User.find().then(users => {
+        res.json({ "users": users })
+    }).catch(err => {
+        res.json({ "error": err })
     })
 
 
 })
 
 //Register ----------------------------------------------------------------------------------
-app.post('/api/register', async(req, res) => {
+app.post('/api/register', async (req, res) => {
 
-    const { username, password, email,waddress } = req.body;
-    console.log(username, password, email,waddress)
+    const { username, password, email, waddress } = req.body;
+    console.log(username, password, email, waddress)
 
-    if (!username || !password || !email ||!waddress) {
+    if (!username || !password || !email || !waddress) {
         return res.status(400).json({ msg: "Please enter all fields" })
     }
 
     //check user already exists
-    const isUserExists = await User.findOne({email})
+    const isUserExists = await User.findOne({ email })
     console.log(isUserExists)
 
-    if(isUserExists){
-        return res.status(400).json({"message":"User already exists! Please Login!"})
+    if (isUserExists) {
+        return res.status(400).json({ "message": "User already exists! Please Login!" })
     }
 
-    const encPass = await bcrypt.hash(password,10)
+    const encPass = await bcrypt.hash(password, 10)
 
     const user = new User({
-        username:username,
-        password:encPass,
-        email:email,
-        waddress:waddress
-})
+        username: username,
+        password: encPass,
+        email: email,
+        waddress: waddress
+    })
 
     user.save().then(user => {
-        res.status(200).json({"user":user.username})
+        res.status(200).json({ "user": user.username })
     }).catch(err => {
         res.status(400).json({ "error": `Error is ${err}` })
     })
@@ -75,30 +78,30 @@ app.post('/api/register', async(req, res) => {
 
 
 // login ----------------------------------------------------------------------------------
-app.post("/api/login",async(req,res)=>{
-    const  {username,password} = req.body;
+app.post("/api/login", async (req, res) => {
+    const { username, password } = req.body;
 
-    if(!username ||!password){
-        return res.status(400).json({msg:"Please enter all fields"})
+    if (!username || !password) {
+        return res.status(400).json({ msg: "Please enter all fields" })
     }
 
-    const user  = await User.findOne({username}).lean()
+    const user = await User.findOne({ username }).lean()
 
 
     //If user not present
-    if(!user){
-        return res.status(400).json({"message":"Invalid Email!"})
+    if (!user) {
+        return res.status(400).json({ "message": "Invalid Email!" })
     }
 
-    const isPasswordCorrect = await bcrypt.compare(password,user.password)
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
     //check password
-    if(isPasswordCorrect){
+    if (isPasswordCorrect) {
         console.log(bcrypt.compare(password, user.password))
         console.log(username)
-        return res.status(200).json({"message":"Login Successfull!","username":user.username,"password":user.password})
+        return res.status(200).json({ "message": "Login Successfull!", "username": user.username, "password": user.password })
     }
-    
-    return res.status(400).json({"error":"Invalid Credentials!"})
+
+    return res.status(400).json({ "error": "Invalid Credentials!" })
 
 
 
