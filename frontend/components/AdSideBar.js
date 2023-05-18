647923c2a89990512900f76db1baea8f01fc26d8
ompanchwate2003 <ompanchwate2003@gmail.com>
2023-05-18 19:42:35 +0530

    Profile and advertisement

--- frontend/components/AdSideBar.js
+++ frontend/components/AdSideBar.js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router';
 import useRequireAuth from '@/utils/useRequireAuth';
 
 
-export default function Sidebar({ isOpen }) {
+export default function AdSidebar({ isOpen }) {
   const router = useRouter();
 
 
@@ -44,21 +44,21 @@ export default function Sidebar({ isOpen }) {
             <ul className="space-y-2 pt-8 font-medium ">
 
               <li>
-                <a href="/" className="flex items-center p-2 text-gray-950 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <a href="#" className="flex items-center p-2 text-gray-950 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <HiHome className='text-2xl text-gray-500' />
                   <span className="ml-3">Home</span>
                 </a>
               </li>
 
               <li>
-                <a href="#" className="flex md:hidden items-center p-2 text-gray-950 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <a href="#" className="flex items-center p-2 text-gray-950 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <span><MdPostAdd className='text-2xl text-gray-500' /></span>
-                  <span className="ml-3">New Post</span>
+                  <span className="ml-3">Add Avertisement</span>
                 </a>
               </li>
 
               <li>
-                <a href="" className="flex items-center p-2 text-gray-950 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <a href="#" className="flex items-center p-2 text-gray-950 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-950 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
                   <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
                 </a>
@@ -66,7 +66,7 @@ export default function Sidebar({ isOpen }) {
 
 
               <li>
-                <a href="/profile" className="flex items-center p-2 text-gray-950 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <a href="#" className="flex items-center p-2 text-gray-950 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-950 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
                   <span className="flex-1 ml-3 whitespace-nowrap">Profile</span>
                 </a>
--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,13 +1,40 @@
 import Layout from "@/components/Layout";
 
 export default function Profile() {
-    return (
+    return (<>
         <Layout>
-            <div className="container">
-                <h1>Profile</h1>
+            <div className="container mt-24 p-5 rounded-xl w-full  dark:bg-gray-900 dark:border-slate-700  flex flex-col items-center">
+                <div className="flex flex-col justify-center items-center">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" className="w-8 h-8 sm:w-28 sm:h-28" />
+                    <h1 className="text-2xl mt-2">@username</h1>
+                    <h1>0xA080c2F113C06d909517d2448eF5A620a16289Eb</h1>
+                </div>
+                <div className="mt-7 text-xl w-[40%]">
+                    <div className="flex justify-between items-center text-gray-200 tracking-wide">
+                        <h1>Name : Robert Downey Junior</h1>
+                        <div className="flex flex-col justify-center items-center border-l pl-10">
+                            <h1 className="text-2xl">50</h1>
+                            <h1 className="text-md">POSTS</h1>
+                        </div>
+                    </div>
+                </div>
+
+
+
             </div>
 
+
         </Layout>
+
+        {/* POSTS HERE */}
+        <div className="p-4 lg:ml-64 dark:bg-gray-950">
+            <div className="container dark:bg-gray-900 dark:border-slate-700 rounded-xl p-5">
+                POSTS HERE...
+            </div>
+        </div>
+
+
+    </>
     )
 
 }   
